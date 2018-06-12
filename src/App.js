024c97e4d74569b59ef802318ec3c217069c8a9b
Vero Hillebrand <veronika.hillebrand@device-insight.com>
2018-06-12 14:24:37 +0200

    New App page

--- src/App.js
+++ src/App.js
@@ -12,21 +12,6 @@ class App extends Component {
 					<h1 className="App-title">Welcome to Device Insight</h1>
 				</header>
 				<div className="App-intro">
-					<p>
-						Your challenge - should you choose to accept it - is to
-						build a "state-of-the art" encryption tool! ;)
-					</p>
-					<p>
-						In the Form component, you will find an input and an
-						output element. A person (let's call him a secret agent)
-						can enter a message. The message should then be reversed
-						and the reversed message shown in the output box.
-					</p>
-					<h4>Example</h4>
-					<h5>Input</h5>
-					<pre>This is a test - is this thing on?</pre>
-					<h5>Output</h5>
-					<pre>sihT si a tset - si siht gniht ?no</pre>
 					<Form />
 				</div>
 			</div>
