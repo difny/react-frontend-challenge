e2cc68eb7bb3499e6333c78742f958829d43707e
Florian Nykrin <florian.nykrin@device-insight.com>
2020-08-11 16:12:45 +0200

    fix form: class -> className

--- src/Form.js
+++ src/Form.js
@@ -3,11 +3,11 @@ import React from 'react';
 export default function Form() {
 	return (
 		<form>
-			<div class="box form-field">
+			<div className="box form-field">
 				<label> INPUT </label>
 				<textarea rows="8" value="This is where the user enters the message" />
 			</div>
-			<div class="box">
+			<div className="box">
 				<strong> OUTPUT </strong>
 				<pre>This is where the reversed string should be displayed</pre>
 			</div>
