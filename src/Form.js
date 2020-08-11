1991325fc7343149effefd56571953cbac273c9a
Florian Nykrin <florian.nykrin@device-insight.com>
2020-08-11 14:18:03 +0200

    update react versions, add prettier

--- src/Form.js
+++ src/Form.js
@@ -6,16 +6,11 @@ export default class Form extends React.Component {
 			<form>
 				<div class="box form-field">
 					<label> INPUT </label>
-					<textarea
-						rows="8"
-						value="This is where the user enters the message"
-					/>
+					<textarea rows="8" value="This is where the user enters the message" />
 				</div>
 				<div class="box">
 					<strong> OUTPUT </strong>
-					<pre>
-						This is where the reversed string should be displayed
-					</pre>
+					<pre>This is where the reversed string should be displayed</pre>
 				</div>
 			</form>
 		);
