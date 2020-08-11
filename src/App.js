5c8c8c9fe66715ca6b6a31e4223c7242ca50e8d7
Florian Nykrin <florian.nykrin@device-insight.com>
2020-08-11 14:20:59 +0200

    use hooks, css formatting

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
-import React, {Component} from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Form from './Form';
 
-class App extends Component {
-	render() {
-		return (
-			<div className="App">
-				<header className="App-header">
-					<img src={logo} className="App-logo" alt="logo" />
-					<h1 className="App-title">Welcome to Device Insight</h1>
-				</header>
-				<div className="App-intro">
-					<Form />
-				</div>
+export default function App() {
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<h1 className="App-title">Welcome to Device Insight</h1>
+			</header>
+			<div className="App-intro">
+				<Form />
 			</div>
-		);
-	}
+		</div>
+	);
 }
-
-export default App;
--- src/Form.js
+++ src/Form.js
@@ -1,18 +1,16 @@
-import * as React from 'react';
+import React from 'react';
 
-export default class Form extends React.Component {
-	render() {
-		return (
-			<form>
-				<div class="box form-field">
-					<label> INPUT </label>
-					<textarea rows="8" value="This is where the user enters the message" />
-				</div>
-				<div class="box">
-					<strong> OUTPUT </strong>
-					<pre>This is where the reversed string should be displayed</pre>
-				</div>
-			</form>
-		);
-	}
+export default function Form() {
+	return (
+		<form>
+			<div class="box form-field">
+				<label> INPUT </label>
+				<textarea rows="8" value="This is where the user enters the message" />
+			</div>
+			<div class="box">
+				<strong> OUTPUT </strong>
+				<pre>This is where the reversed string should be displayed</pre>
+			</div>
+		</form>
+	);
 }
